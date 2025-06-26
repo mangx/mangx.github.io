502396b20a65e2ccb8e104c01512ebff14e6248d
mangx <49263713+mangx@users.noreply.github.com>
2025-06-26 20:02:51 +0800

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -76,8 +76,8 @@ for row, item in publications.iterrows():
     
     md += """\npermalink: /publication/""" + html_filename
     
-    if len(str(item.funding)) > 5:
-        md += "\nfunding: '" + html_escape(item.funding) + "'"
+    if len(str(item.excerpt)) > 5:
+        md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
     md += "\ndate: " + str(item.pub_date) 
     
@@ -95,8 +95,8 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
-    if len(str(item.funding)) > 5:
-        md += "\n" + html_escape(item.funding) + "\n"
+    if len(str(item.excerpt)) > 5:
+        md += "\n" + html_escape(item.excerpt) + "\n"
         
     md += "\nRecommended citation: " + item.citation
     
